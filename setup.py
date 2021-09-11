3a0e8a35deebfd1d0b41b71f9c9a7d85983e4c12
lingulist <mattis.list@lingulist.de>
2021-09-11 09:43:44 +0200

    update new files

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("metadata.json", encoding="utf-8") as fp:
 
 
 setup(
-    name="lexibank_lamayi",
+    name="lexibank_lamanisoic",
     description=metadata["title"],
     license=metadata.get("license", ""),
     url=metadata.get("url", ""),
-    py_modules=["lexibank_lamayi"],
+    py_modules=["lexibank_lamanisoic"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={"lexibank.dataset": ["leejaponic=lexibank_lamayi:Dataset"]},
+    entry_points={"lexibank.dataset": ["leejaponic=lexibank_lamanisoic:Dataset"]},
     install_requires=["pylexibank>=2.1"],
     extras_require={"test": ["pytest-cldf"]},
 )
