4dc2a49b3b9bb05fe7fa52fec74a7e5504e3a3af
lingulist <mattis.list@lingulist.de>
2021-09-11 09:20:00 +0200

    update new files

---
+++ test.py
@@ -0,0 +1,19 @@
+def test_valid(cldf_dataset, cldf_logger):
+    assert cldf_dataset.validate(log=cldf_logger)
+
+
+def test_forms(cldf_dataset):
+    assert len(list(cldf_dataset["FormTable"])) == 11681
+    assert any(f["Form"] == "phet55" for f in cldf_dataset["FormTable"])
+
+
+def test_languages(cldf_dataset, cldf_logger):
+    assert len(list(cldf_dataset["LanguageTable"])) == 41
+
+
+def test_parameters(cldf_dataset, cldf_logger):
+    assert len(list(cldf_dataset["ParameterTable"])) == 301
+
+
+def test_sources(cldf_dataset, cldf_logger):
+    assert len(cldf_dataset.sources) == 1
